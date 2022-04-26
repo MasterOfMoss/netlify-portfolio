dd754ae88f3d738e3e5c811619099dcad3d2561b
Jesse Moss <lokmer27@hotmail.com>
2022-04-25 21:05:41 -0700

    Contact.js update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -54,7 +54,7 @@ export default function Contact() {
                             <h2 className="title-font font-semibold text-white tracking-widest text-cs">
                                 EMAIL
                             </h2>
-                            <a className="text-indigo-400 leading-relaxed">
+                            <a href="" className="text-indigo-400 leading-relaxed">
                                 lokmer27@hotmail.com
                             </a>
                             <h2 className="titlefont font-semibold text-white tracking-widest text-xs mt-4">
