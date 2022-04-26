e549e83fa2d62192b70ee555430bd657490ecacb
Jesse Moss <lokmer27@hotmail.com>
2022-04-25 21:23:49 -0700

    Updated Skills and added data.js

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description: "Here will rest the first project to grace our portfolio page!",
+        image: "./image1.gif",
+        link: "https://freecodecamp.org",
+    },
+    {
+        title: "React ToDo List",
+        subtitle: "MERN Stack",
+        description: "ToDo App might be a good idea for one of the projects",
+        image: "./image1.gif",
+        link: "https://freecodecamp.org",
+    },
+];
+export const skills = ["HTML", "CSS", "Bootstrap", "JavaScript", "React", "Tailwind",];
